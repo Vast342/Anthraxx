643a506c33454fe8a4736340ec54b833a50c2f8e
Vast342 <78440113+Vast342@users.noreply.github.com>
2024-01-16 08:17:06 -0500

    Board Stuff

--- src/board.cpp
+++ src/board.cpp
@@ -2,7 +2,7 @@
 #include "board.h"
 #include "move.h"
 
-void Board::makeMove(Move move) {
+void Board::makeMove(const Move move) {
     stateHistory.push_back(currentState);
     int startSquare = move.getStartSquare();
     int endSquare = move.getEndSquare();
@@ -29,3 +29,51 @@ void Board::undoMove() {
     currentState = stateHistory.back();
     stateHistory.pop_back();
 }
+
+void Board::addTile(const int square) {
+    assert(square < 49);
+    assert(tileAtIndex(square) == None);
+    const uint64_t squareAsBitboard = 1ULL << square;
+    currentState.bitboards[currentState.sideToMove] ^= squareAsBitboard;
+}
+
+void Board::removeTile(const int square) {
+    assert(square < 49);
+    assert(tileAtIndex(square) == currentState.sideToMove);
+    const uint64_t squareAsBitboard = 1ULL << square;
+    currentState.bitboards[currentState.sideToMove] ^= squareAsBitboard;
+}
+
+void Board::flipTile(const int square) {
+    assert(square < 49);
+    assert(tileAtIndex(square) != None);
+    const uint64_t squareAsBitboard = 1ULL << square;
+    currentState.bitboards[currentState.sideToMove] ^= squareAsBitboard;
+    currentState.bitboards[1 - currentState.sideToMove] ^= squareAsBitboard;
+}
+
+void Board::flipNeighboringTiles(const int square) {
+    assert(square < 49);
+    uint64_t neighbors = (bitboards[1 - currentState.sideToMove] & neighboringTiles[square]);
+    assert(((bitboards[X] & bitboards[O]) & neighbors) == neighbors);
+    
+    while(neighbors != 0) {
+        int index = popLSB(neighbors);
+        flipTile(index);
+    }
+}
+
+int Board::tileAtIndex(const int square) const {
+    assert(square < 49);
+    const uint64_t squareAsBitboard = 1ULL << square;
+    for(int i = 0; i < 2; i++) {
+        if((currentState.bitboards[i] & squareAsBitboard) != 0) {
+            return i;
+        }
+    }
+    return 2;
+}
+
+int Board::evaluate() {
+    return __builtin_popcountll(bitboards[currentState.sideToMove]) - __builtin_popcountll(bitboards[1 - currentState.sideToMove])
+};
