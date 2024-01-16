d907059352d12c9462ef7abece41fb5fa1ab4413
Vast342 <78440113+Vast342@users.noreply.github.com>
2024-01-16 08:31:59 -0500

    Better Expand

--- src/lookups.h
+++ src/lookups.h
@@ -2,16 +2,31 @@
 
 #include "global_includes.h"
 
+constexpr uint64_t fileMask = 0b1000000100000010000001000000100000010000001;
+constexpr uint64_t fileMask = 0b1111111;
+
+constexpr uint64_t getFileMask(const int file) {
+    return fileMask << file;
+}
+
+constexpr uint64_t getRankMask(const int rank) {
+    return fileMask << (7 * rank);
+};
+
 constexpr uint64_t expand(const int square) {
     const uint64_t squareAsBitboard = 1ULL << square;
-    return squareAsBitboard << 1
-         & squareAsBitboard >> 1
-         & squareAsBitboard << 7
-         & squareAsBitboard >> 7
-         & squareAsBitboard << 6
-         & squareAsBitboard >> 6
-         & squareAsBitboard << 8
-         & squareAsBitboard >> 8;
+    const uint64_t allButLeftMask = ~getFileMask(0);
+    const uint64_t allButRightMask = ~getFileMask(6);
+    const uint64_t allButTopMask = ~getRankMask(6);
+    const uint64_t allButBottomMask = ~getRankMask(0); 
+    return squareAsBitboard << 1 & allButRightMask
+         & squareAsBitboard >> 1 & allButLeftMask
+         & squareAsBitboard << 7 & allButBottomMask
+         & squareAsBitboard >> 7 & allButTopMask
+         & squareAsBitboard << 6 & (allButBottomMask & allButRightMask)
+         & squareAsBitboard >> 6 & (allButTopMask & allButLeftMask)
+         & squareAsBitboard << 8 & (allButBottomMask & allButLeftMask)
+         & squareAsBitboard >> 8 & (allButTopMask & allButRightMask);
 }
 
 constexpr std::array<uint64_t, 49> generateExpanded() {
