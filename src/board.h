525a6d08311934b4a769cb730e28f0168d60fa59
Vast342 <78440113+Vast342@users.noreply.github.com>
2024-01-16 08:04:17 -0500

    CONST, CONST, CONST

--- src/board.h
+++ src/board.h
@@ -7,7 +7,7 @@ enum Bitboards {
 };
 
 enum Colors {
-    Black, White
+    Black, White, None
 };
 
 struct BoardState {
@@ -20,16 +20,17 @@ struct BoardState {
 
 struct Board {
     public:
-        Board(std::string fen);
-        int getMoves(std::array<Move, 194> moves);
-        void makeMove(Move move);
+        Board(const std::string fen);
+        int getMoves(std::array<Move, 194> &moves);
+        void makeMove(const Move move);
         void undoMove();
+        int evaluate();
     private:
         BoardState currentState;
         std::vector<BoardState> stateHistory;
-        void addTile(int square);
-        void removeTile(int square);
-        void flipTile(int square);
-        void flipNeighboringTiles(int square);
-        int tileAtIndex(int square);
+        void addTile(const int square);
+        void removeTile(const int square);
+        void flipTile(const int square);
+        void flipNeighboringTiles(const int square);
+        int tileAtIndex(const int square) const;
 };
