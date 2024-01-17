79a70a830ca1d89525569d56a2ce3a3f7eec81c7
Vast342 <78440113+Vast342@users.noreply.github.com>
2024-01-17 11:26:58 -0500

    string split

--- src/global_includes.h
+++ src/global_includes.h
@@ -22,3 +22,18 @@ inline int popLSB(uint64_t &bitboard) {
     bitboard &= bitboard - 1;
     return lsb;
 }
+
+// splits a string into segments based on the seperator
+inline std::vector<std::string> split(const std::string string, const char seperator) {
+    std::stringstream stream(string);
+    std::string segment;
+    std::vector<std::string> list;
+
+    // every time that it can get a segment
+    while(std::getline(stream, segment, seperator)) {
+        // add it to the vector
+        list.push_back(segment);
+    }
+    
+    return list;
+}
